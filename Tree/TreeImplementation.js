f1413fa5a99707b7079332936e11988ceffd051d
Rakibul Islam <hridoy.9@gmail.com>
2021-07-17 12:50:21 +0600

    TreeImplementation.js-->remove() added, insert() updated

--- Tree/TreeImplementation.js
+++ Tree/TreeImplementation.js
@@ -11,26 +11,28 @@ class Node {
         this.root = null;
     }
     insert(value){
-        let currentNode = this.root;
         const newNode = new Node(value);
-        if (currentNode === null) {
+        if (this.root === null) {
             this.root = newNode;
-            return this;
-        }
-        while (true) {
-            if (value < currentNode.value) {
-                if (currentNode.left === null) {
+        } else {
+            let currentNode = this.root;
+             while(true){
+                if(value < currentNode.value){
+                //Left
+                if(!currentNode.left){
                     currentNode.left = newNode;
                     return this;
                 }
                 currentNode = currentNode.left;
             } else {
-                if (currentNode.right === null) {
+                //Right
+                if(!currentNode.right){
                     currentNode.right = newNode;
                     return this;
-                }
+                } 
                 currentNode = currentNode.right;
             }
+          }
         }
     }
     lookup(value){
@@ -53,6 +55,94 @@ class Node {
         return "Value not found";
     }
     // remove
+    remove(value) {
+        if (!this.root) {
+            return false; 
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while(currentNode){
+            if(value < currentNode.value){
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if(value > currentNode.value){
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                //We have a match, get to work!
+                
+                //Option 1: No right child: 
+                if (currentNode.right === null) {
+                    if (parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+                    
+                        //if parent > current value, make current left child a child of parent
+                        if(currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+                    
+                        //if parent < current value, make left child a right child of parent
+                        } else if(currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+                
+                //Option 2: Right child which doesnt have a left child
+                } else if (currentNode.right.left === null) {
+                    currentNode.right.left = currentNode.left;
+                    if(parentNode === null) {
+                    this.root = currentNode.right;
+                    } else {
+                    
+                    //if parent > current, make right child of the left the parent
+                    if(currentNode.value < parentNode.value) {
+                        parentNode.left = currentNode.right;
+                    
+                    //if parent < current, make right child a right child of the parent
+                    } else if (currentNode.value > parentNode.value) {
+                        parentNode.right = currentNode.right;
+                    }
+                }
+                
+                //Option 3: Right child that has a left child
+                } else {
+        
+                    //find the Right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while(leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+                
+                    //Parent's left subtree is now leftmost's right subtree
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+        
+                    if(parentNode === null) {
+                        this.root = leftmost;
+                    } else {
+                        if(currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        } else if(currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
+    // findReplacingNode(currentNode) {
+    //     if (currentNode.right !== null) {
+    //         currentNode = currentNode.right;
+    //     }
+    //     while (currentNode.left !== null) {
+    //         currentNode = currentNode.left;
+    //     }
+    //     return currentNode;
+    // }
   }
   
   const tree = new BinarySearchTree();
@@ -63,9 +153,10 @@ class Node {
   tree.insert(170)
   tree.insert(15)
   tree.insert(1)
+  //tree.remove(20);
   let t = JSON.stringify(traverse(tree.root))
   console.log(t);
-  console.log(tree.lookup(111));
+//  console.log(tree.lookup(111));
 
   
   //     9
