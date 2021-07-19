2ab0854bf458def6481bdd623a7403f17f9448a1
Rakibul Islam <hridoy.9@gmail.com>
2021-07-19 23:08:58 +0600

    Insertion Sort implementation added O(n^2)

---
+++ Sorting/InsertionSort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        public static List<int> insertionSort(List<int> numbers) {
+            int item;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < numbers[0])
+                {
+                    item = numbers[i];
+                    numbers.RemoveAt(i);
+                    numbers.Insert(0,item);
+                }
+                else
+                {
+                    for (int j = 1; j < i; j++)
+                    {
+                        if (numbers[i] > numbers[j-1] && numbers[i] < numbers[j])
+                        {
+                            item = numbers[j];
+                            numbers.RemoveAt(i);
+                            numbers.Insert(j, item);
+                        }
+                    }
+                }
+            }
+            return numbers;
+        }
+
+        public static string Display(int[] numbers) {
+            string nums = null;
+            foreach (var item in numbers)
+            {
+                nums += item.ToString() + " ";
+            }
+            return nums;
+        }
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+            //numbers.Add(33);
+            //numbers.Insert(1, 33);            
+            numbers = insertionSort(numbers);
+
+            numbers.ForEach(num => Console.Write(num + " "));
+
+            //Console.WriteLine(Display(insertionSort(numbers))); 
+
+            // Time Complexity: 
+            // Space Complexity: 
+        }
+    }
+}
