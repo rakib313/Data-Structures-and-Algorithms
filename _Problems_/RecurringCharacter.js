01cf057316cac8b232a6325814b68e451e5affc4
Rakibul Islam <hridoy.9@gmail.com>
2021-07-29 18:19:49 +0600

    RecurringCharacter.js -> Effencincy increased by sorting which increases best case

--- _Problems_/RecurringCharacter.js
+++ _Problems_/RecurringCharacter.js
@@ -10,7 +10,7 @@
 //Given an array = [2,3,4,5]:
 //It should return undefined
 
-
+var input = [2,1,1,2,3,5,1,2,4];
 function firstRecurringCharacter(input) {
     const col = new Map();
     for (let i = 0; i < input.length; i++) {
@@ -24,8 +24,31 @@ function firstRecurringCharacter(input) {
     return "Recurring character does not exist";
 }
 
-let ans = firstRecurringCharacter([2,1,1,2,3,5,1,2,4]) // Time -> O(n) solution, Space O(n)
+let ans = firstRecurringCharacter(input) // Time -> O(n) solution, Space O(n)
 console.log(ans);
+
+
+// 
+function firstRecurringCharacter2(input) {
+    input.sort((a,b) => {
+        return a-b;
+    });
+    if(input.length <= 1) {
+        return false;
+    } else {
+        for (let i = 0; i < input.length; i++) {
+            if (input[i] === input[i+1]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+if (firstRecurringCharacter2(input)) {
+    console.log("true");
+} else console.log("false");
+
+
 //Bonus... What if we had this:
 // [2,5,5,2,3,5,1,2,4]
 // return 5 because the pairs are before 2,2
